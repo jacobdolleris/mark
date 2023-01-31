1797efae8b808d0d1f8523609ed09399bbd7c592
Jacob Dolleris <J.dolleris@hotmail.com>
2023-01-31 11:20:08 +0100

    done

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import { marked } from "marked";
-document.getElementById('parsed').innerHTML =
-marked.parse('# Marked in the browser');
+
+
+var area = document.getElementById('markdown');
+if (area.addEventListener){
+    area.addEventListener('input',function(){
+        var text = document.getElementById("markdown").value; 
+        document.getElementById('parsed').innerHTML =
+        marked.parse(text);
+    },false);
+}
